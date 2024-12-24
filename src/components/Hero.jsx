9aba5e2ef0f90cce4f848c7a58218ef7c566f1cf
ShadySoltan <shady.aly.soltan@gmail.com>
2024-12-24 11:35:34 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
                 <p>Welcome To</p>
                 <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Evolve <span className='text-blue-400'>Fitness</span></h1>
             </div>
-            <p className='text-sm md:text-base font-light'>I fully embrace the possibility of transforming into a <span className='text-blue-400 font-medium'>upowerhouse of strength and size</span> , recognizing the risks of
+            <p className='text-sm md:text-base font-light'>I fully embrace the possibility of transforming into a <span className='text-blue-400 font-medium'>powerhouse of strength and size</span> , recognizing the risks of
             becoming a walking colossus. I accept the challenge of pushing my limits, knowing it may lead to outgrowing
             my wardrobe, redefining door frames, and facing the mirror with awe and disbelief. <span className='text-blue-400 font-medium'>With unwavering determination</span>, I welcome the 
             journey to become a force to be reckoned with, accepting every hardship as part of the glory of ultimate transformation.</p>
